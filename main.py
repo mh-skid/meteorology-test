9dddd5e01e369d84beef8dd20ba86485b69e26a9
mh-skid <158527649+mh-skid@users.noreply.github.com>
2024-02-21 19:30:32 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#pip install python-weather
+#pip install pygame
+
+if __name__=="__main__":
+  import python_weather
+  import pygame
+
+  import asyncio
+  import os
+
+  async def getweather():
+    # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+    async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+      # fetch a weather forecast from a city
+      weather = await client.get('New York')
+      
+      # returns the current day's forecast temperature (int)
+      print(weather.current.temperature)
+      
+      # get the weather forecast for a few days
+      for forecast in weather.forecasts:
+        print(forecast)
+        
+        # hourly forecasts
+        for hourly in forecast.hourly:
+          print(f' --> {hourly!r}')
+  
+  #shameless api rip lmao
+  #add ui with pygame later
+  #kapeesh
